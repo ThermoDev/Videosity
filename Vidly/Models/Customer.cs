a0b8d9e18aa9ce2ce0c8fa015d94cc2c2208ac90
ThermoLink <newbleter@gmail.com>
2017-11-18 20:51:29 +1100

    Overriding Conventions

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Videosity.Models {
     public class Customer {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool isSubscribed { get; set; }
         // Navigation property
